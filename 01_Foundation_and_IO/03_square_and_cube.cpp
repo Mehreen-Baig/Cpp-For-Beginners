4b4586c34089dc3c6fa21bb35008dc6e1ca5e674
Mehreen-Baig <mehreenbaig735@gmail.com>
2026-06-17 01:54:41 +0500

    Create 03_square_and_cube.cpp

---
+++ 01_Foundation_and_IO/03_square_and_cube.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num,square,cube;
+	cout << "Enter a number";
+	cin >> num;
+	square = num * num;
+	cube = num * num * num;
+ cout<<"Square is"<<"="<<square << "\n";
+ cout << "Cube is"<<"=" << cube;
+}
