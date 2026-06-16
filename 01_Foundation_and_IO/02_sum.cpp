4996e53b57ec7a3ff92d2a7eb9323a64bc87a089
Mehreen-Baig <mehreenbaig735@gmail.com>
2026-06-17 01:48:58 +0500

    Create 02_sum.cpp

---
+++ 01_Foundation_and_IO/02_sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2, num3, num4, sum;
+    cout << "Enter four numbers: ";
+    cin >> num1 >> num2 >> num3 >> num4;
+
+    sum = num1 + num2 + num3 + num4;
+    cout << "Sum: " << sum;
+    return 0;
+}
