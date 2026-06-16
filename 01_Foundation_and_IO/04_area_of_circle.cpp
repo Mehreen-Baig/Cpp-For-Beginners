42bb13c8778cb37f77e669fdf7e0fc7d40ac04c5
Mehreen-Baig <mehreenbaig735@gmail.com>
2026-06-17 02:17:20 +0500

    Create 04_area_of_circle.cpp

---
+++ 01_Foundation_and_IO/04_area_of_circle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double area, radius;
+		cout << "Enter radius";
+		cin >> radius;
+		area = radius * radius * 3.14159;
+		cout <<"Area of Circle is="<<area;
+		return 0;
+}
